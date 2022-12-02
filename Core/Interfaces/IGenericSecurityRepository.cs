252a3d94b1d74920d697f80d0d018c9a801601ab
toni <toni.one.dev@gmail.com>
2022-12-02 19:41:40 +0100

    Creacion repositorio genérico seguridad, interfaz specificaciones y instalacion automapper

---
+++ Core/Interfaces/IGenericSecurityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericSecurityRepository<T> where T : IdentityUser
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> GetByIdWithSpecAsync(ISpecification<T> spec);
+
+        Task<IReadOnlyList<T>> GetAllWithSpecAsync(ISpecification<T> spec);
+
+        Task<int> CountAsync(ISpecification<T> spec);
+
+        Task<int> Add(T entity);
+
+        Task<int> Update(T entity);
+
+        Task<int> Delete(int id);
+    }
+}
